08bbde1bf15bb93e006a9da3c74dcd1c931cca37
Pavel Abdrashitov <pavel.abdrashitov@gmail.com>
2019-01-09 21:25:35 +0700

    Заявки // Добавил селект с пунктом "Назад" - o-select_choice

--- js/functions.js
+++ js/functions.js
@@ -4,24 +4,40 @@ $(document).ready(function () {
   var content = link.import.querySelector('#system-panel_content');
   document.querySelector('#system-panel').appendChild(content.cloneNode(true));
 
-  $(".o-container__title").click(function(){
+  $(".o-container__title").click(function() {
     var parent = $(this).parent();
     $(".o-container__item").not(parent).removeClass('o-container__item_opened')
     parent.toggleClass('o-container__item_opened')
   });
 
-  $('.o-switcher').click(function(){
+  $('.o-switcher').click(function() {
     $(this).toggleClass('o-switcher_active')
   });
 
-  $('.o-icon_more').click(function(){
+  $('.o-icon_more').click(function() {
     $(this).next('.o-dropdown-list').slideToggle(300);
   });
 
-  $('.o-dropdown-list__item_close').click(function(){
+  $('.o-dropdown-list__item_close').click(function() {
     $(this).parent('.o-dropdown-list').slideUp(300);
     return false;
   });
+
+  initSelect();
 });
+
+function initSelect() {
+  var select = $('.o-select');
+  select.each(function () {
+    $(this).on("click", function() {
+      select.not(this).removeClass('o-select_opened');
+      $(this).addClass('o-select_opened');
+    });
+  });
+  $('.o-select__item_back').click(function() {
+    select.removeClass('o-select_opened');
+    return false;
+  })
+}
 
 
